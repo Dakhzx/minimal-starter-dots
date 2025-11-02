a23af7b24f136248eda0e6d205ae0d3d60a4959e
Dakhz <n72824822@gmail.com>
2025-11-02 13:05:31 +0530

    Update black.sh

--- config/theme-switcher/black.sh
+++ config/theme-switcher/black.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "aplying theme..."
 
-swww img /home/dakhs/minimal-starter-dots/wallpapers/cube_mono.png
-wal -i /home/dakhs/minimal-starter-dots/wallpapers/cube_mono.png
+swww img /minimal-starter-dots/wallpapers/cube_mono.png
+wal -i /minimal-starter-dots/wallpapers/cube_mono.png
 pkill waybar
-waybar -c /home/dakhs/.config/waybar/config.jsonc -s /home/dakhs/.config/waybar/minimal-square.css &
+waybar -c .config/waybar/config.jsonc -s .config/waybar/minimal-square.css &
 disown
 clear
 fastfetch
